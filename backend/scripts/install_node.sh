565a62f2d49892fcae2092e2b94a74961dd42232
lalitpatil56 <lalit.patil5694@gmail.com>
2025-04-25 17:31:40 -0400

    edit script

--- backend/scripts/install_node.sh
+++ backend/scripts/install_node.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+
 echo "Installing Node.js 16 from Amazon Linux Extras..."
 sudo yum clean all
 sudo yum update -y
 sudo amazon-linux-extras enable nodejs16
 sudo yum install -y nodejs
 
+# Refresh shell path in non-interactive script
+export PATH=$PATH:/usr/bin
+
 echo "Verifying Node.js installation..."
+which node
 node -v
+which npm
 npm -v
