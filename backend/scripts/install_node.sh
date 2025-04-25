45c375b7ba96b81e4ba2c533b678d1a573a6a448
lalitpatil56 <lalit.patil5694@gmail.com>
2025-04-25 17:24:44 -0400

    edit scripts

--- backend/scripts/install_node.sh
+++ backend/scripts/install_node.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-
-# Exit immediately if a command exits with a non-zero status
-set -e
-
-# Install Node.js 18 from NodeSource
-curl -fsSL https://rpm.nodesource.com/setup_18.x | sudo bash -
+sudo yum update -y
+sudo amazon-linux-extras enable nodejs16
 sudo yum install -y nodejs
+
 
 # Check version
 node -v
