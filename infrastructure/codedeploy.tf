7181a21c5891dd9302828a70ae80ee8be3cf35ac
lalitpatil56 <lalit.patil5694@gmail.com>
2025-04-24 22:25:00 -0400

    edit role

--- infrastructure/codedeploy.tf
+++ infrastructure/codedeploy.tf
@@ -52,5 +52,5 @@ resource "aws_iam_role" "codedeploy_role" {
 
 resource "aws_iam_role_policy_attachment" "codedeploy_attach" {
   role       = aws_iam_role.codedeploy_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AdministratorAccess"
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
