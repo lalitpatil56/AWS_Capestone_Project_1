760e4d6ba215b368d18bd4ee89e95cd3b6bdb80d
lalitpatil56 <lalit.patil5694@gmail.com>
2025-04-25 00:37:41 -0400

    increase asg ec2 instance

--- infrastructure/ec2_asg.tf
+++ infrastructure/ec2_asg.tf
@@ -134,9 +134,9 @@ resource "aws_lb_listener" "webapp_http" {
 
 # Auto Scaling Group
 resource "aws_autoscaling_group" "webapp_asg" {
-  desired_capacity     = 1
-  max_size             = 1
-  min_size             = 1
+  desired_capacity     = 2
+  max_size             = 2
+  min_size             = 2
   vpc_zone_identifier  = [aws_subnet.public_a.id, aws_subnet.public_b.id]
   target_group_arns    = [aws_lb_target_group.webapp_tg.arn]
   launch_template {
