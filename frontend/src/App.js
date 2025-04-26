6e76794dce6ae1c02732c29217d77c27d251872f
lalitpatil56 <lalit.patil5694@gmail.com>
2025-04-25 21:59:00 -0400

    edit app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
-import UsersTable from './UsersTable';
+import UsersTable from './UserTable';
 
 function App() {
   const [formData, setFormData] = useState({
